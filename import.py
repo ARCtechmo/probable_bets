0eb1cda69d2ecb15464348b4bef5fcdabf9a9678
ARCtechmo <arctitan78@gmail.com>
2023-09-15 11:26:44 -0400

    added functions to parse the ESPN urls; successful importation and creation of .json files

--- import.py
+++ import.py
@@ -13,7 +13,6 @@
 #  -each "athlete" has a unique identifieer called "id" and must be included along with the player name and league 
 
 # Task: modify the code as necessary to download the .json files from the API
-## 4for4 Air Yards API ##
 
 ## FantasyPros projections API ## 
 # https://api.fantasypros.com/public/v2/json/nfl/{season}/projections
@@ -22,9 +21,12 @@
 # https://www.pff.com/api/betting/best_bets?league=nfl
 
 # Task: update code to scrape the html (requests or beautifulsoup)
+
 ## thelines.com ## game totals and implied team totals
 # https://www.thelines.com/betting/nfl/implied-team-totals/
 
+## 4for4 Air Yards ##
+# https://www.4for4.com/tools/air-yards
 
 # import the libraries
 import requests
@@ -32,8 +34,7 @@ import json
 import csv
 import random
 import time
-
-
+from urllib.parse import urlparse, parse_qs
 
 # Make an HTTP GET request
 def fetch_data(url):
@@ -66,6 +67,25 @@ def write_standard_csv(data,file_name):
         for player_data in data["resultSet"]["rowSet"]:
                 csv_writer.writerow(player_data)
 
+# parse the espn urls, name, and dump into json files
+def parse_espn_url(url):
+
+        # Formulate JSON filename based on URL components
+        query_string = urlparse(url).query
+        params = parse_qs(query_string)
+        sort_param = params.get('sort', [''])[0]
+        page_param = params.get('page',[''])[0]
+        season_param = params.get('season', [''])[0]
+
+        if sort_param and page_param and season_param:
+
+            # Access the first element from list and then split at %
+            # split and replace special characters
+            sort_value = sort_param.split('%')[0]
+            sort_value = sort_value.replace(':desc','').replace('.','_')
+            return f"espn_{sort_value}_page{page_param}_{season_param}.json"
+        return None
+
 if __name__ == "__main__":
 
     # list of URLs
@@ -79,20 +99,28 @@ if __name__ == "__main__":
             "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=false&page=1&limit=50&category=scoring&sort=scoring.totalPoints%3Adesc&season=2022&seasontype=2",
             "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=false&page=1&limit=50&category=specialTeams%3Areturning&sort=returning.kickReturnYards%3Adesc&season=2022&seasontype=2",
             "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=false&page=1&limit=50&category=specialTeams%3Akicking&sort=kicking.fieldGoalsMade%3Adesc&season=2022&seasontype=2",
-            "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=true&page=1&limit=50&category=specialTeams%3Apunting&sort=punting.grossAvgPuntYards%3Adesc&season=2022&seasontype=2"
-    ]
+            "https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=true&page=1&limit=50&category=specialTeams%3Apunting&sort=punting.grossAvgPuntYards%3Adesc&season=2022&seasontype=2",
+
+            ]
     
-    for index, url in enumerate(urls):
+    # loop through the urls
+    for url in urls:
 
         # Randomized time delay between 1 and 6 seconds
-        time_delay = random.uniform(1,6)
+        # fetch the urls and assign to a variable        
+        time_delay = random.uniform(1,3)
         time.sleep(time_delay)
-
         data = fetch_data(url)
-    
+
+        # test urls string splicing without fetching
+        # data = urls
+
         if data:
-            if "resultSet" in data and "rowSet" in data["resultSet"]:
-                write_standard_csv(data, 'nfl_data.csv')
-            else:
-                with open(f"nfl_stats_{index}.json", "w") as txtfile:
-                    json.dump(data, txtfile, indent=4)
+            if 'espn' in url:
+                json_file_name = parse_espn_url(url)
+                if json_file_name:
+                    with open(json_file_name, "w") as txtfile:
+                        json.dump(data, txtfile, indent=4)
+        
+        # elif "resultSet" in data and "rowSet" in data["resultSet"]:
+        #     write_standard_csv(data, 'nfl_data.csv')
