12123042b45b5442498b25a272d8b133790dc8d8
ARCtechmo <arctitan78@gmail.com>
2023-10-03 14:15:42 -0400

    completed functions to extract the json data into nested lists

--- fp_parse.py
+++ fp_parse.py
@@ -2,9 +2,6 @@
 # Notes on the json file
 # NOTE: player and team ids do not match ESPN ids so do not use these for fk
 
-
-# Task: modify the code to read multiple files
-
 # loop over the dictionary and pull the keys and values
 import json
 
@@ -17,6 +14,7 @@ def read_json(file):
 def extract_player_data(players):
     extracted_data = []
     unique_fpids = set()
+    unique_rows = set()
     
     for player in players:
         fpid = player['fpid']
@@ -34,6 +32,13 @@ def extract_player_data(players):
         stats = player['stats']
         
         player_data = [name, position_id, team_id] + [stats[key] for key in stats.keys()]
+        
+        # Skip duplicate rows based on player_data
+        player_data_tuple = tuple(player_data)
+        if player_data_tuple in unique_rows:
+            continue
+
+        unique_rows.add(player_data_tuple)
         extracted_data.append(player_data)
     
     return extracted_data
@@ -52,7 +57,6 @@ def main():
     'fantasy_pros_WR_projections_2023_STD.json'
     ]
 
-    # Task: add check to ensure key lenght equals value length
     for file in files:
 
         # file = 'fantasy_pros_QB_projections_2023_STD.json'
@@ -64,41 +68,80 @@ def main():
         if position == 'DST':
             DST_key_values = get_key_values(data['players'][0])
             DST_extracted_data = extract_player_data(data['players'])
+
+            # Check if key length matches data length for each player
+            if all(len(DST_key_values) == len(player_data) for player_data in DST_extracted_data):
+                print(f"Key lengths match value lengths for {position}.")
+            else:
+                print(f"Key lengths do NOT match value lengths for {position}.")
+
             # print(f"Key Values for {position}:", DST_key_values)  
             # print(f"Extracted Data for {position}:", DST_extracted_data[:1])  
         
-
         elif position == 'K':
             K_key_values = get_key_values(data['players'][0])
             K_extracted_data = extract_player_data(data['players'])
+
+            # Check if key length matches data length for each player
+            if all(len(K_key_values) == len(player_data) for player_data in K_extracted_data):
+
+                print(f"\nKey lengths match value lengths for {position}.")
+            else:
+                print(f"\nKey lengths do NOT match value lengths for {position}.")
+
             # print(f"Key Values for {position}:", K_key_values)  
-            # print(f"\nExtracted Data for {position}:", K_extracted_data[:1]) 
+            # print(f"Extracted Data for {position}:", K_extracted_data[:1]) 
 
         elif position == 'QB':
             QB_key_values = get_key_values(data['players'][0])
             QB_extracted_data = extract_player_data(data['players'])
+
+            # Check if key length matches data length for each player
+            if all(len(QB_key_values) == len(player_data) for player_data in QB_extracted_data):
+                print(f"\nKey lengths match value lengths for {position}.")
+            else:
+                print(f"\nKey lengths do NOT match value lengths for {position}.")
+
             # print(f"Key Values for {position}:", QB_key_values)  
-            # print(f"\nExtracted Data for {position}:", QB_extracted_data[:1])
+            # print(f"Extracted Data for {position}:", QB_extracted_data[:1])
 
         elif position == 'RB':
             RB_key_values = get_key_values(data['players'][0])
             RB_extracted_data = extract_player_data(data['players'])
-            print(f"Key Values for {position}:", RB_key_values)  
-            print(f"\nExtracted Data for {position}:", RB_extracted_data[:1])
+
+            # Check if key length matches data length for each player
+            if all(len(RB_key_values) == len(player_data) for player_data in RB_extracted_data):
+                print(f"\nKey lengths match value lengths for {position}.")
+            else:
+                print(f"\nKey lengths do NOT match value lengths for {position}.")
+            
+            # print(f"Key Values for {position}:", RB_key_values)  
+            # print(f"Extracted Data for {position}:", RB_extracted_data[:1])
 
         elif position == 'TE':
             TE_key_values = get_key_values(data['players'][0])
             TE_extracted_data = extract_player_data(data['players'])
-            print(f"Key Values for {position}:", TE_key_values)  
-            print(f"\nExtracted Data for {position}:", TE_extracted_data[:1])
+
+            # Check if key length matches data length for each player
+            if all(len(TE_key_values) == len(player_data) for player_data in TE_extracted_data):
+                print(f"\nKey lengths match value lengths for {position}.")
+            else:
+                print(f"\nKey lengths do NOT match value lengths for {position}.")
+            
+            # print(f"Key Values for {position}:", TE_key_values)  
+            # print(f"Extracted Data for {position}:", TE_extracted_data[:1])
 
         elif position == 'WR':
             WR_key_values = get_key_values(data['players'][0])
             WR_extracted_data = extract_player_data(data['players'])
-            print(f"Key Values for {position}:", WR_key_values)  
-            print(f"\nExtracted Data for {position}:", WR_extracted_data[:1])
 
-        
+            # Check if key length matches data length for each player
+            if all(len(WR_key_values) == len(player_data) for player_data in WR_extracted_data):
+                print(f"\nKey lengths match value lengths for {position}.")
+            else:
+                print(f"\nKey lengths do NOT match value lengths for {position}.")
+            print(f"Key Values for {position}:", WR_key_values)  
+            print(f"Extracted Data for {position}:", WR_extracted_data[:1])
 
 if __name__ == "__main__":
     main()
