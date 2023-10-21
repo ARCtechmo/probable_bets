8b5929281c1e0bdc22c9c9509ceafa30c604b550
ARCtechmo <arctitan78@gmail.com>
2023-10-21 11:05:47 -0400

    code to create the database and functions to export the data

--- database.py
+++ database.py
@@ -6,60 +6,80 @@ def create_connection():
     conn = sqlite3.connect('database.db')
     return conn
 
-def create_table(conn, create_table_sql):
-    """Create a table using the provided SQL statement."""
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except sqlite3.Error as e:
-        print(e)
+def create_tables(conn):
+    cur = conn.cursor()
+    cur.executescript(
+        '''
+        CREATE TABLE IF NOT EXISTS league(
+        id INTEGER NOT NULL PRIMARY KEY,
+        league TEXT NOT NULL UNIQUE
+        );
 
-def main():
-    # SQL statement to drop old "offense" table (optional)
-    drop_old_offense_table = """ DROP TABLE IF EXISTS offense; """
+        CREATE TABLE IF NOT EXISTS season(
+        id INTEGER NOT NULL PRIMARY KEY,
+        seasonType TEXT NOT NULL UNIQUE
+        );
 
-    # SQL statement to create updated "offense" table
-    # offense_table = """ CREATE TABLE IF NOT EXISTS offense (
-    #                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #                         name TEXT NOT NULL,
-    #                         team TEXT,
-    #                         games_played INTEGER,
-    #                         total_yards INTEGER,
-    #                         total_yards_per_game REAL,
-    #                         total_passing_yards INTEGER,
-    #                         total_passing_yards_per_game REAL,
-    #                         total_rushing_yards INTEGER,
-    #                         total_rushing_yards_per_game REAL,
-    #                         total_points INTEGER,
-    #                         total_points_per_game REAL
-    #                     ); """
+        CREATE TABLE IF NOT EXISTS athletes(
+        id INTEGER NOT NULL PRIMARY KEY,
+        uid INTEGER NOT NULL UNIQUE,
+        guid INTEGER NOT NULL UNIQUE,
+        sport TEXT NOT NULL,
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL
+        );
 
-    # SQL statement to create "defense" table
-    # defense_table = """ CREATE TABLE IF NOT EXISTS defense (
-    #                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #                         name TEXT NOT NULL,
-    #                         rating INTEGER
-    #                     ); """
-    
-    # Create a database connection
+        CREATE TABLE IF NOT EXISTS positions(
+        id INTEGER NOT NULL PRIMARY KEY,
+        position TEXT NOT NULL,
+        abbr TEXT NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS teams(
+        id INTEGER NOT NULL PRIMARY KEY,
+        teamUid INTEGER NOT NULL UNIQUE,
+        teamName TEXT NOT NULL UNIQUE,
+        teamShortName TEXT NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS athleteStatus(
+        id INTEGER NOT NULL PRIMARY KEY,
+        status TEXT NOT NULL
+        )
+       
+        '''
+    )
+    conn.commit()
+    conn.close()
+
+def insert_into_league(conn, league_data):
+    cur = conn.cursor()
+    for data in league_data:
+        if isinstance(data, (list, tuple)) and len(data) == 2:
+            id, league_name = data
+            cur.execute("SELECT * FROM league WHERE id=?", (id,))
+            if cur.fetchone() is None:
+                cur.execute("INSERT INTO league (id, league) VALUES (?, ?)", (id, league_name))
+        else:
+            print(f"Skipping invalid data: {data}")
+    conn.commit()
+
+def insert_into_season(conn, season_data):
+    cur = conn.cursor()
+    for data in season_data:
+        if isinstance(data, (list, tuple)) and len(data) == 2:
+            id, season_type = data
+            cur.execute("SELECT * FROM season WHERE id=?", (id,))
+            if cur.fetchone() is None:
+                cur.execute("INSERT INTO season (id, seasonType) VALUES (?,?)", (id, season_type))
+        else:
+            print(f"Skipping invalid data: {data}")
+    conn.commit()
+## TASK: create the remaining functions to export data to the db
+
+if __name__ == '__main__':
+    # Create a connection to the database
     conn = create_connection()
 
     # Create tables
-    if conn is not None:
-        # Drop the old "offense" table to create the new one
-        c = conn.cursor()
-        c.execute(drop_old_offense_table)
-
-        # create_table(conn, offense_table)
-        # create_table(conn, defense_table)
-
-        # Commit changes
-        conn.commit()
-
-        # Close resources
-        conn.close()
-    else:
-        print("Error! Cannot establish a database connection.")
-
-if __name__ == '__main__':
-    main()
+    create_tables(conn)
