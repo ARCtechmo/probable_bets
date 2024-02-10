b40fb1c65e1f7b671be499079402277172fba24e
ARCtechmo <arctitan78@gmail.com>
2024-02-10 11:22:44 -0500

    tasking notes

--- pff_parse.py
+++ pff_parse.py
@@ -1,8 +1,17 @@
 ## parse the pff json file ##
 
 # Notes on the json file
+# NOTE: the json file has two sections: game props and player props 
 # NOTE: some player "projections" props missing becuase odds are available 24 to 36 hours prior to kickoff.
 # NOTE: player and team ids do not match ESPN ids so do not use these for fk
+# NOTE: you will need two tables: pff_player_props, pff_team_props
+
+## task: we will only use player_props
+## task: create the lists from the keys
+## task: "player props" keys: player_full_name, team_name, opponent_team_name, prop_type, line, over, under, 
+## task: create functions similar to the functions in fp_parse4.py to get player and team ids
+## task: create the lists with the correct foreign keys
+## task: create the tables and functions in database.py
 
 # loop over the dictionary and pull the keys and values
 import json
@@ -25,7 +34,7 @@ def extract_props_last_updated_at(data):
 
 def extract_player_props(data):
     keys_order = ["props_last_updated_at", "player_full_name", "position", "team_name", "opponent_team_name",
-                  "prop_type", "projection", "line", "over", "over_value", "under", "under_value"]
+                  "prop_type", "projection", "line", "over", "under"]
 
     player_props_list = []
     for player_prop in data.get('player_props', []):
