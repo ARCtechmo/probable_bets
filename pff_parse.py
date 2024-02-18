0deebc8e21a0888e14aa39df4d68ee1242f490b6
ARCtechmo <arctitan78@gmail.com>
2024-02-18 14:24:12 -0500

    finalized functions to create the array of lists with the matched foreign keys for the athletes, teams, and positions

--- pff_parse.py
+++ pff_parse.py
@@ -1,17 +1,14 @@
 ## parse the pff json file ##
 
 # Notes on the json file
+# NOTE: you will need two tables: pff_player_props, pff_team_props
 # NOTE: the json file has two sections: game props and player props 
 # NOTE: some player "projections" props missing becuase odds are available 24 to 36 hours prior to kickoff.
-# NOTE: player and team ids do not match ESPN ids so do not use these for fk
-# NOTE: you will need two tables: pff_player_props, pff_team_props
 
-## task: we will only use player_props
-## task: create the lists from the keys
-## task: "player props" keys: player_full_name, team_name, opponent_team_name, prop_type, line, over, under, 
-## task: create functions similar to the functions in fp_parse4.py to get player and team ids
-## task: create the lists with the correct foreign keys
+
+## task: test to ensure all of the foreign keys match the players, teams, positions with multiple json files
 ## task: create the tables and functions in database.py
+## NOTE: you will need to allow NULL values for the athlete
 
 # loop over the dictionary and pull the keys and values
 from database import create_connection
@@ -57,7 +54,7 @@ def extract_player_props(data):
     return keys_order, player_props_list
 
 
-# match the player to the athlete foriegn ke
+# match the player to the athlete foriegn keys
 def query_athlete_id(conn, standardized_name, suffix='', team=None, position=None):
     # Initialize cursor
     cursor = conn.cursor()
@@ -127,6 +124,7 @@ def query_athlete_id(conn, standardized_name, suffix='', team=None, position=Non
     # No match found or multiple ambiguous matches
     return None
 
+# NOTE: this function must be placed after the query_athlete_id() function
 def insert_athlete_id(conn, player_props_list):
     # Iterate over player_props_list and update with athlete IDs
     updated_player_props_list = []
@@ -138,10 +136,106 @@ def insert_athlete_id(conn, player_props_list):
 
         # Replace player name with athlete ID in the list
         updated_prop = player_prop.copy()
-        updated_prop[1] = athlete_id if athlete_id else player_prop[1]  # Keep original name if ID not found
+
+        # Keep original name if ID not found
+        # updated_prop[1] = athlete_id if athlete_id else player_prop[1]
+
+        # Return None if ID not found
+        updated_prop[1] = athlete_id if athlete_id is not None else None    
 
         updated_player_props_list.append(updated_prop)
     return updated_player_props_list
+
+## function to match teams
+# NOTE: this function must be placed after the insert_athlete_id() function
+def match_and_replace_team_abbreviations(conn, player_props_list):
+
+    # First, insert athlete IDs into the player props list
+    updated_player_props_list_athlete_id = insert_athlete_id(conn, player_props_list)
+
+    # Dictionary to handle variations in team abbreviations
+    # ARI vs ARZ, CLE vs CLV, HOU vs HST, JAX VS JAC, LAR vs LA, WSH vs WAS
+    team_abbreviation_variations = {
+        "ARZ": "ARI",
+        "CLV": "CLE",
+        "HST": "HOU",
+        "JAC": "JAX",
+        "LA": "LAR",
+        "WAS": "WSH"
+    }
+
+    # Query the database for team short names and their corresponding IDs
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, teamShortName FROM teams")
+    team_data = cursor.fetchall()
+    
+    # Map teamShortName to team ID
+    team_id_map = {team[1]: team[0] for team in team_data}  
+
+    # Function to get the team ID using abbreviation
+    def get_team_id(abbreviation):
+
+        # Correct the abbreviation if there's a known variation
+        corrected_abbreviation = team_abbreviation_variations.get(abbreviation, abbreviation)
+        
+        # Return the team ID for the corrected abbreviation
+        return team_id_map.get(corrected_abbreviation)
+
+    # Replace team abbreviations in the list with corresponding team IDs
+    for prop in updated_player_props_list_athlete_id:
+        team_abbr = prop[3]  
+        opponent_abbr = prop[4]  
+
+        # Get the team ID for both team and opponent
+        team_id = get_team_id(team_abbr)
+        opponent_team_id = get_team_id(opponent_abbr)
+
+        # Replace the abbreviations with team IDs in the list
+        prop[3] = team_id
+        prop[4] = opponent_team_id
+
+    return updated_player_props_list_athlete_id
+
+
+## function to match positions
+# NOTE: this function must be placed after the match_and_replace_team_abbreviations() function
+def match_and_replace_position_abbreviations(conn, player_props_list):
+
+    # insert athlete and team IDs into the player props list
+    updated_player_props_list_athlete_team_id = match_and_replace_team_abbreviations(conn, player_props_list)
+
+    # Mapping to handle variations in position abbreviations
+    # position abbreviation variations: K vs PK
+    position_abbreviation_variations = {"K": "PK"}
+
+    # Query the database for position names and their corresponding IDs
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, abbr FROM positions")
+    position_data = cursor.fetchall()
+
+    # Mapping of team abbreviation to position ID
+    position_id_map = {position[1]: position[0] for position in position_data} 
+
+    # Function to get the position ID using abbreviation
+    def get_position_id(abbreviation):
+
+        # Correct the abbreviation if there's a known variation
+        corrected_abbreviation = position_abbreviation_variations.get(abbreviation, abbreviation)
+        
+        # Return the position ID for the corrected abbreviation
+        return position_id_map.get(corrected_abbreviation)
+
+    # Replace position abbreviations in the list with corresponding position IDs
+    for prop in updated_player_props_list_athlete_team_id:
+        position_abbr = prop[2]  
+
+        # Get the position ID
+        position_id = get_position_id(position_abbr)
+
+        # Replace the abbreviation with the position ID in the list
+        prop[2] = position_id
+
+    return updated_player_props_list_athlete_team_id
 
 def main():
     conn = create_connection()
@@ -171,12 +265,22 @@ def main():
     # print("\nKeys:", keys_order)
     # print("\nValues:", player_props_list[0])
 
+    ## test loop ##
     # updated_player_props_list
-    updated_player_props_list = insert_athlete_id(conn, player_props_list)
-    # print("\nUpdated Values:", updated_player_props_list[0])
+    # updated_player_props_list_athlete_id = insert_athlete_id(conn, player_props_list)
+    # for player in updated_player_props_list_athlete_id:
+    #     print(player)
+    
+    ## test loop ##
+    # updated_player_props_list
+    # updated_player_props_list_athlete_team_id = match_and_replace_team_abbreviations(conn, player_props_list)
+    # for player in updated_player_props_list_athlete_team_id:
+    #     print(player)
 
     ## test loop ##
-    for player in updated_player_props_list:
+    # final_updated_player_props_list
+    final_updated_player_props_list_athlete_team_position_id = match_and_replace_position_abbreviations(conn, player_props_list)
+    for player in final_updated_player_props_list_athlete_team_position_id:
         print(player)
 
 if __name__ == "__main__":
