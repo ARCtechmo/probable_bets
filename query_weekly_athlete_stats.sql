516dcf0c0346019bc860f074c0c858e0d572db27
ARCtechmo <arctitan78@gmail.com>
2024-06-29 13:10:21 -0400

    modified query to reflect accurate week-by-week data

--- query_weekly_athlete_stats.sql
+++ query_weekly_athlete_stats.sql
@@ -1,66 +1,141 @@
-/*query returns weekly athlete stats */
+/*returns week-by-week stats for the player*/
+/*note: correctly reflects the weekly data*/
+WITH cumulative_stats AS (
+    SELECT
+        ps1.weekStart,
+        ps1.weekEnd,
+        CAST(SUBSTR(ps1.week, 6) AS INTEGER) - 1 AS adjusted_week,
+        a.firstName,
+        a.lastName,
+        pos.abbr AS position,
+        ps1.PassingTouchdowns,
+        ps1.PassingYards,
+        ps1.PassingAttempts,
+        ps1.Completions,
+        ps1.Interceptions,
+        ps1.RushingAttempts,
+        ps1.RushingYards,
+        ps1.RushingTouchdowns,
+        ps1.Rushing1stDowns,
+        ps1.RushingFumbles,
+        ps1.RushingFumblesLost,
+        ps1.Receptions,
+        ps1.ReceivingYards,
+        ps1.ReceivingTouchdowns,
+        ps1.ReceivingTargets,
+        ps1.ReceivingYardsAfterCatch,
+        ps1.ReceivingFirstDowns,
+        ps1.Yards20PlusReceivingPlays,
+        ps1.FieldGoalAttempts,
+        ps1.FieldGoalMade,
+        ps1.ExtraPointsMade,
+        ps1.playerFK
+    FROM
+        playerStatistics ps1
+    JOIN 
+        athletes a ON ps1.playerFK = a.id
+    JOIN 
+        positions pos ON ps1.PlayerPositionFK = pos.id
+    WHERE
+        LOWER(a.firstName) = LOWER('lamar') /*input first name*/
+        AND LOWER(a.lastName) = LOWER('jackson') /*input last name*/
+        AND strftime('%Y', ps1.weekStart) = '2023' /*input year*/
+        AND ps1.week != 'Week 6'  /*PLACEHOLDER FOR 2023 DATA - REMOVE LATER*/
+      
+),
+previous_stats AS (
+    SELECT
+        CAST(SUBSTR(ps1.week, 6) AS INTEGER) AS week,
+        ps1.playerFK,
+        SUM(ps1.PassingTouchdowns) AS PassingTouchdowns,
+        SUM(ps1.PassingYards) AS PassingYards,
+        SUM(ps1.PassingAttempts) AS PassingAttempts,
+        SUM(ps1.Completions) AS Completions,
+        SUM(ps1.Interceptions) AS Interceptions,
+        SUM(ps1.RushingAttempts) AS RushingAttempts,
+        SUM(ps1.RushingYards) AS RushingYards,
+        SUM(ps1.RushingTouchdowns) AS RushingTouchdowns,
+        SUM(ps1.Rushing1stDowns) AS Rushing1stDowns,
+        SUM(ps1.RushingFumbles) AS RushingFumbles,
+        SUM(ps1.RushingFumblesLost) AS RushingFumblesLost,
+        SUM(ps1.Receptions) AS Receptions,
+        SUM(ps1.ReceivingYards) AS ReceivingYards,
+        SUM(ps1.ReceivingTouchdowns) AS ReceivingTouchdowns,
+        SUM(ps1.ReceivingTargets) AS ReceivingTargets,
+        SUM(ps1.ReceivingYardsAfterCatch) AS ReceivingYardsAfterCatch,
+        SUM(ps1.ReceivingFirstDowns) AS ReceivingFirstDowns,
+        SUM(ps1.Yards20PlusReceivingPlays) AS Yards20PlusReceivingPlays,
+        SUM(ps1.FieldGoalAttempts) AS FieldGoalAttempts,
+        SUM(ps1.FieldGoalMade) AS FieldGoalMade,
+        SUM(ps1.ExtraPointsMade) AS ExtraPointsMade
+    FROM
+        playerStatistics ps1
+    JOIN 
+        athletes a ON ps1.playerFK = a.id
+    JOIN 
+        positions pos ON ps1.PlayerPositionFK = pos.id
+    WHERE
+        LOWER(a.firstName) = LOWER('lamar') /*input first name*/
+        AND LOWER(a.lastName) = LOWER('jackson') /*input last name*/
+        AND strftime('%Y', ps1.weekStart) = '2023' /*input year*/
+        AND ps1.week != 'Week 6' /*PLACEHOLDER FOR 2023 DATA - REMOVE LATER*/
+        
+    GROUP BY
+        week, ps1.playerFK
+)
 SELECT
-    ps.weekStart,
-    ps.weekEnd,
-    ps.week,
-    a.firstName,
-    a.lastName,
-    pos.abbr AS position,
-    ps.PassingTouchdowns AS passing_touchdowns,
-    ps.TotalQBR,
-    ps.PassingYards AS passing_yards,
-    ps.PassingAttempts AS passing_attempts,
-    ps.Completions AS completions,
-    ps.Interceptions AS interceptions,
-    ps.RushingAttempts AS rushing_attempts,
-    ps.RushingYards AS rushing_yards,
-    ps.RushingTouchdowns AS rushing_touchdowns,
-    ps.Rushing1stDowns AS rushing_first_downs,
-    ps.RushingFumbles AS rushing_fumbles,
-    ps.RushingFumblesLost AS rushing_fumbles_lost,
-    ps.Receptions AS receptions,
-    ps.ReceivingYards AS receiving_yards,
-    ps.ReceivingTouchdowns AS receiving_touchdowns,
-    ps.ReceivingTargets AS receiving_targets,
-    ps.ReceivingYardsAfterCatch AS receiving_yards_after_catch,
-    ps.ReceivingFirstDowns AS receiving_first_downs,
-    ps.Yards20PlusReceivingPlays AS twenty_plus_yards_receiving_plays,
-    ps.FieldGoalAttempts AS field_goal_attempts,
-    ps.FieldGoalMade AS field_goals_made,
-    ps.ExtraPointsMade AS extra_points_made
+    cs.weekStart,
+    cs.weekEnd,
+    'Week ' || cs.adjusted_week AS week,
+    cs.firstName,
+    cs.lastName,
+    cs.position,
+    cs.PassingTouchdowns - COALESCE(ps.PassingTouchdowns, 0) AS PassingTouchdowns,
+    cs.PassingYards - COALESCE(ps.PassingYards, 0) AS PassingYards,
+    cs.PassingAttempts - COALESCE(ps.PassingAttempts, 0) AS PassingAttempts,
+    cs.Completions - COALESCE(ps.Completions, 0) AS Completions,
+    cs.Interceptions - COALESCE(ps.Interceptions, 0) AS Interceptions,
+    cs.RushingAttempts - COALESCE(ps.RushingAttempts, 0) AS RushingAttempts,
+    cs.RushingYards - COALESCE(ps.RushingYards, 0) AS RushingYards,
+    cs.RushingTouchdowns - COALESCE(ps.RushingTouchdowns, 0) AS RushingTouchdowns,
+    cs.Rushing1stDowns - COALESCE(ps.Rushing1stDowns, 0) AS Rushing1stDowns,
+    cs.RushingFumbles - COALESCE(ps.RushingFumbles, 0) AS RushingFumbles,
+    cs.RushingFumblesLost - COALESCE(ps.RushingFumblesLost, 0) AS RushingFumblesLost,
+    cs.Receptions - COALESCE(ps.Receptions, 0) AS Receptions,
+    cs.ReceivingYards - COALESCE(ps.ReceivingYards, 0) AS ReceivingYards,
+    cs.ReceivingTouchdowns - COALESCE(ps.ReceivingTouchdowns, 0) AS ReceivingTouchdowns,
+    cs.ReceivingTargets - COALESCE(ps.ReceivingTargets, 0) AS ReceivingTargets,
+    cs.ReceivingYardsAfterCatch - COALESCE(ps.ReceivingYardsAfterCatch, 0) AS ReceivingYardsAfterCatch,
+    cs.ReceivingFirstDowns - COALESCE(ps.ReceivingFirstDowns, 0) AS ReceivingFirstDowns,
+    cs.Yards20PlusReceivingPlays - COALESCE(ps.Yards20PlusReceivingPlays, 0) AS Yards20PlusReceivingPlays,
+    cs.FieldGoalAttempts - COALESCE(ps.FieldGoalAttempts, 0) AS FieldGoalAttempts,
+    cs.FieldGoalMade - COALESCE(ps.FieldGoalMade, 0) AS FieldGoalMade,
+    cs.ExtraPointsMade - COALESCE(ps.ExtraPointsMade, 0) AS ExtraPointsMade
 FROM
-    playerStatistics ps
-JOIN 
-    athletes a ON ps.playerFK = a.id
-JOIN 
-    positions pos ON ps.PlayerPositionFK = pos.id
+    cumulative_stats cs
+LEFT JOIN 
+    previous_stats ps ON cs.adjusted_week = ps.week AND cs.playerFK = ps.playerFK
 WHERE
-    LOWER(a.firstName) = LOWER('patrick') /*manually change last name*/
-    AND LOWER(a.lastName) = LOWER('mahomes') /*manually change first name*/
-    AND strftime('%Y', ps.weekStart) = '2023' /*manually change year*/
-    AND NOT (
-        ps.PassingTouchdowns IS NULL AND
-        ps.TotalQBR IS NULL AND
-        ps.PassingYards IS NULL AND
-        ps.PassingAttempts IS NULL AND
-        ps.Completions IS NULL AND
-        ps.Interceptions IS NULL AND
-        ps.RushingAttempts IS NULL AND
-        ps.RushingYards IS NULL AND
-        ps.RushingTouchdowns IS NULL AND
-        ps.Rushing1stDowns IS NULL AND
-        ps.RushingFumbles IS NULL AND
-        ps.RushingFumblesLost IS NULL AND
-        ps.Receptions IS NULL AND
-        ps.ReceivingYards IS NULL AND
-        ps.ReceivingTouchdowns IS NULL AND
-        ps.ReceivingTargets IS NULL AND
-        ps.ReceivingYardsAfterCatch IS NULL AND
-        ps.ReceivingFirstDowns IS NULL AND
-        ps.Yards20PlusReceivingPlays IS NULL AND
-        ps.FieldGoalAttempts IS NULL AND
-        ps.FieldGoalMade IS NULL AND
-        ps.ExtraPointsMade IS NULL
-    )
-ORDER BY
-    CAST(SUBSTR(ps.week, 6) AS INTEGER);
+    cs.PassingTouchdowns IS NOT NULL OR
+    cs.PassingYards IS NOT NULL OR
+    cs.PassingAttempts IS NOT NULL OR
+    cs.Completions IS NOT NULL OR
+    cs.Interceptions IS NOT NULL OR
+    cs.RushingAttempts IS NOT NULL OR
+    cs.RushingYards IS NOT NULL OR
+    cs.RushingTouchdowns IS NOT NULL OR
+    cs.Rushing1stDowns IS NOT NULL OR
+    cs.RushingFumbles IS NOT NULL OR
+    cs.RushingFumblesLost IS NOT NULL OR
+    cs.Receptions IS NOT NULL OR
+    cs.ReceivingYards IS NOT NULL OR
+    cs.ReceivingTouchdowns IS NOT NULL OR
+    cs.ReceivingTargets IS NOT NULL OR
+    cs.ReceivingYardsAfterCatch IS NOT NULL OR
+    cs.ReceivingFirstDowns IS NOT NULL OR
+    cs.Yards20PlusReceivingPlays IS NOT NULL OR
+    cs.FieldGoalAttempts IS NOT NULL OR
+    cs.FieldGoalMade IS NOT NULL OR
+    cs.ExtraPointsMade IS NOT NULL
+ORDER BY 
+    cs.adjusted_week;
